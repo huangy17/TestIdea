454b85627384f78d1f35181659fbd57df0227a18
huangy17 <huangy17@miamioh.edu>
2021-07-07 21:32:54 +0800

    Second push

--- Module01/src/com/atguigu/bean/Customer.java
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -7,7 +7,7 @@ package com.atguigu.bean;
 public class Customer {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!!");
 
     }
 
