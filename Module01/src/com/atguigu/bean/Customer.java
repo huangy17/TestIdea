d1b65c901b84a821f6385c72ef7ebcd7df47aae0
huangy17 <huangy17@miamioh.edu>
2021-07-07 21:52:28 +0800

    Third push

--- Module01/src/com/atguigu/bean/Customer.java
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -8,7 +8,12 @@ public class Customer {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+
         System.out.println("Hello World2");
+        System.out.println("Hello World2");
+        System.out.println("Hello World2");
+
 
     }
 
